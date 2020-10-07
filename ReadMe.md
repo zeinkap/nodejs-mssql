7c5c7977c2a8bcf63f57f9754062b1536cdfc078
zeinkap <zeinkap@gmail.com>
2020-10-07 16:52:46 -0400

    inserting fake data to DB using faker

--- ReadMe.md
+++ ReadMe.md
@@ -1,6 +1,4 @@
 # IMPORTANT
-* prior to executing code ensure that Microsft SQL server is configured correctly 
+* prior to executing code, ensure that Microsft SQL Server is configured correctly 
     * TCP/IP is enabled in SQL Server Configuration Manger for both server and client
-    * Ensure  that ‘SQL Server Browser’ service is running
-    * Using SQL Server Authentication credentials
-    * Database exists and contains data
+    * SQL Server Browser service is running
--- app.js
+++ app.js
@@ -2,31 +2,46 @@ require("dotenv").config();
 const express = require('express');
 const app = express();
 const sql = require("mssql");
+const faker = require('faker');
 
 // config DB
 const config = {
     server: 'localhost',
     user: process.env.USER,
     password: process.env.PASSWORD,
-    database: "Learning"
+    database: 'Learning'
 };
 
+function generateCustomer() {
+    console.log(faker.name.firstName());
+    console.log(faker.name.lastName());
+    console.log(faker.internet.email());
+}
+
+function generateOrder() {
+    console.log(faker.date.past());
+    console.log(faker.commerce.price());
+    console.log(faker.random.number());
+}
+
+generateOrder();
+generateCustomer();
+
 app.get('/', (req, res) => {
-    // connect to DB
-    sql.connect(config, (err) => {
+    // connect to MSSQL DB
+    let connection = new sql.ConnectionPool(config, (err) => {
         if(err) {
             console.log("Error connecting to DB see msg below");
             console.log(err);
         }
-        // create Request object
-        let request = new sql.Request();
+        // create Request object so we can send sql queries 
+        let request = new sql.Request(connection);
         // query to DB
-        request.query("SELECT * FROM customers", (err, recordset) => {
+        request.query("SELECT c.first_name, c.last_name, c.email, o.order_date, o.amount FROM customers c JOIN orders o ON o.customer_id = c.id", (err, recordset) => {
             if (err) {
                 console.log(err);
             } else {
                 res.send(recordset);
-                console.log("Result: " + recordset);
             }
         });
     });
